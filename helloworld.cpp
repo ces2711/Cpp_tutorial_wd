c83fe15a0e9e94268d4b342021c4e7ba2bc39c7a
ces2711 <129199690+ces2711@users.noreply.github.com>
2025-05-14 08:29:31 -0500

    commit 5.14.25

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+    std::cout << "I like pizza!" << std::endl;
+    std::cout << "It's really good!" << std::endl;
+    return 0;
+}
+
+/* 
+std:cout = standard character output
+<< is the output operator
+character string denoted with "", just like c
+you can use the output operator multiple times
+std::endl = standard end line
+you can do the same thing by using the \n character
+but I suspect there are differences
+\n has better performance
+endline will flush the output buffer (this is important)
+*/
+
+// comments and block comments also appear to work the same
+
